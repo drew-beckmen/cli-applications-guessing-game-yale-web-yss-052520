5884fd82422092dac0328da9d32735a4d10d1b59
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 18:42:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,12 @@
 # Code your solution here!
+def run_guessing_game
+  num = rand(1..6)
+  puts "Guess a number between 1 and 6: "
+  guess = gets 
+  if guess == name
+    puts "You guessed the correct number!"
+  elsif guess == "exit"
+    puts "Goodbye!"
+  else
+    outs "Sorry! The computer guessed #{num}."
+end
